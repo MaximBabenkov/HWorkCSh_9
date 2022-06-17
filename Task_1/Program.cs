4968f573df6456a92b49ec6ad7a21c12982a0396
MaximBabenkov <lostbass@mail.ru>
2022-06-17 20:25:14 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Показать все натуральные числа от m до n, n и m заданы
+
+void NaturNumbers (int m, int n)
+{
+    if (n<m) return;    
+    NaturNumbers(m,n-1);
+    Console.Write(n+" ");
+}
+Console.Clear();
+NaturNumbers(4,15);
